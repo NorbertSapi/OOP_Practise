89359388f558c8c7c54d91cd785782b31eb8f29b
NorbertSapi <39389379+NorbertSapi@users.noreply.github.com>
2019-10-07 23:20:09 +0100

    Add files via upload
    add extra functions

--- Employee.py
+++ Employee.py
@@ -2,33 +2,39 @@
 from Skills import Skills
 from Modell import Modell
 
-worker1 = Modell("Jo", "11014771", 25, "Pick")
+worker1 = Modell("Jo", "110147", 25, "Pick")
 print(worker1.name)
 print(worker1.id)
 print(worker1.age)
 print(worker1.department)
 
-# !!! You have to fix this part!!!
-worker1.__str__
+# two ways to call str method from class Modell
+Modell.str(worker1)
+worker1.str()
 
-# from Skills import Skills
+
 # instantiating the skills class
 person1 = Skills("True", "Pack")
-
+person2 = Skills("True", "Pick")
 print(person1.new_hire)
 print(person1.where_department)
 
+# calling development method from class Skills
+person1.development(True, True, True)
+person2.development(False, True, False)
+
 
 # this is a method which tells you if the training is in progress
+# method from Main class
 def development(learning):
     if learning:
+        print("method from Main class")
         print("learning in progress")
-        # return True
     else:
         return False
 
 
-# calling the development method
+# calling the development method from Main class
 development("True")
 
 print("Hello-GitHub-World")
