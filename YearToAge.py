08a782c1f179d8cde108d9af7e34d3f91f70718d
NorbertSapi <39389379+NorbertSapi@users.noreply.github.com>
2019-10-10 02:03:40 +0100

    Add files via upload
    add an extra function

--- YearToAge.py
+++ YearToAge.py
@@ -6,6 +6,14 @@ from Age import Age
 name = input("What is your name? ")
 print("Hi %s nice to meet you. " % name)
 
+username = input("Enter your username: ")
+print("Username: %s " % username)
+pass_word = input("Enter your password")
+length_password = int(len(pass_word))
+
+print("Your password is {} and your password is {} character long. ".format(pass_word, length_password))
+
+
 birth_year: str = input("When were you born? ")
 print("You were born in %s " % birth_year)
 
